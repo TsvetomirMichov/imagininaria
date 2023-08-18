4cddb8a2e3a136b6acbfb5a9b2d6db7cc7cde749
TsvetomirMichov <94786397+TsvetomirMichov@users.noreply.github.com>
2023-08-18 23:54:41 +0300

    Update BecomeACreator.jsx

--- src/pages/BecomeACreator/BecomeACreator.jsx
+++ src/pages/BecomeACreator/BecomeACreator.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
-import { Box, Typography, Link } from '@mui/material'
+import { Box } from '@mui/material'
 import LoginPage from '../Login/Login'
-import TopImageLeft1 from '../../images/about/image.webp'
-import TopImageLeft2 from '../../images/about/about2.webp'
-import TopImageRight1 from '../../images/about/about1.webp'
-import TopImageRight2 from '../../images/about/about4.webp'
-import logo_about from '../../images/about/logo_about.webp'
+import TopImageLeft2 from '../../images/about/top_right.webp'
+import TopImageRight2 from '../../images/about/top_left.webp'
+import TopImageRight1 from '../../images/about/Imaginaria-about-1.jpg'
 import styled from '@emotion/styled'
 
 
@@ -40,11 +38,12 @@ const BecomeACreator = () => {
         <div>
             <Box sx={{
                 position: 'relative',
+                mb:'15em'
             }}>
                 <Navbar />
                 <Box sx={{
-                    backgroundColor: '#171717',
-                    color: 'white',
+                    backgroundColor: 'white',
+                    color: 'black',
                     width: '100%',
                     height: '100vh',
                     display: 'flex',
@@ -53,27 +52,45 @@ const BecomeACreator = () => {
                 }}>
              
                     <BoxTop sx={{ mt: '10em', padding: '1.5em',display:{xs:'none',sm:'flex'} }}>
-
-                        <Box sx={{ display: 'flex', flexDirection: 'row', mt: '-1em', mr: '1em' }}>
-                            <Box gap={5} mt={2} p={{ xs: 4, sm: 5 }} position={'relative'} sx={{ display: 'flex', flexDirection: 'column' }}>
-                                <ImageContainer>
-                                    <img src={TopImageRight1} width={190} height={230} alt="Image 5" />
+                        <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: '100%',
+                        alignItems: { xs: 'center', sm: 'flex-end' }
+                    }}>
+                        <Box sx={{
+                            display: 'flex', flexDirection: 'row', mt: '-1em', ml: '1em', position: 'relative',
+                            width: '100%',
+                            height: { xs: '40%', sm: '100%' }
+                        }}>
+                            <Box position={'relative'} p={3} sx={{ display: 'flex', flexDirection: 'column', width: '15em', height: '15em' }}>
+                                <ImageContainer sx={{
+                                    width: { xs: '100%', sm: '15em' },
+                                }}>
+                                    <img src={TopImageRight1} width={'100%'} height={'100%'} alt="Image 5" />
                                 </ImageContainer>
-
-                                <img src={TopImageLeft2} width={190} height={230} alt="Image 3" />
-
                             </Box>
-                            <Box gap={5} position={'relative'} sx={{ display: 'flex', flexDirection: 'column' }}>
-                                <ImageContainer>
-                                    <img src={TopImageLeft1} width={190} height={230} alt="Image 5" />
+                            <Box position={'relative'} p={3} mt={4} sx={{ display: 'flex', flexDirection: 'column', width: '15em', height: '5em' }}>
+                                <ImageContainer sx={{
+                                    width: { xs: '100%', sm: '15em' },
+                                }}>
+                                    <img src={TopImageLeft2} width={'100%'} height={'100%'} alt="Image 5" />
                                 </ImageContainer>
 
-                                <img src={TopImageRight2} width={190} height={230} alt="Image 3" />
                             </Box>
                         </Box>
+                        <Box position={'relative'} p={3} >
+                            <ImageContainer sx={{
+                                width: { xs: '100%', sm: '30em' },
+                                height:'20em'
+                            }}>
+                                <img src={TopImageRight2} style={{height:'100%', objectFit: 'cover' }} alt="Image 5" />
+                            </ImageContainer>
+                        </Box>
+                    </Box>
                     </BoxTop>
-                    <LoginPage />
                     
+                    <LoginPage />
                 </Box>
             </Box>
             <Footer />
